60f31dd05d46ecb98312c04c60b57a6daf17e9f4
Steve <steve.gitprofile@outlook.com>
2024-08-25 22:54:43 +0200

    implemented custom hook for fetching single city

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,7 +1,8 @@
-import { BackButton } from "components";
+import { BackButton, Spinner, Error } from "components";
 
 import styles from "./City.module.css";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useFetchCity } from "../../hooks/useFetchCity";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -12,67 +13,53 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
-
-  const { cityName, emoji, date, notes } = currentCity;
-
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+
+  const { city, isLoading, error } = useFetchCity(id);
+
+  const { cityName, emoji, date, notes } = city;
+
+  if (isLoading) return <Spinner />;
+
+  if (error) return <Error />;
 
   return (
-    <>
-      <h1>City with id: {id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-    </>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        <BackButton />
+      </div>
+    </div>
   );
-
-  // return (
-  //   <div className={styles.city}>
-  //     <div className={styles.row}>
-  //       <h6>City name</h6>
-  //       <h3>
-  //         <span>{emoji}</span> {cityName}
-  //       </h3>
-  //     </div>
-
-  //     <div className={styles.row}>
-  //       <h6>You went to {cityName} on</h6>
-  //       <p>{formatDate(date || null)}</p>
-  //     </div>
-
-  //     {notes && (
-  //       <div className={styles.row}>
-  //         <h6>Your notes</h6>
-  //         <p>{notes}</p>
-  //       </div>
-  //     )}
-
-  //     <div className={styles.row}>
-  //       <h6>Learn more</h6>
-  //       <a
-  //         href={`https://en.wikipedia.org/wiki/${cityName}`}
-  //         target="_blank"
-  //         rel="noreferrer"
-  //       >
-  //         Check out {cityName} on Wikipedia &rarr;
-  //       </a>
-  //     </div>
-
-  //     <div>
-  //       <BackButton />
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default City;
--- src/hooks/useFetchCity.jsx
+++ src/hooks/useFetchCity.jsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from "react";
 
-export function useFetchCities() {
-  const [cities, setCities] = useState([]);
+export function useFetchCity(id) {
+  const [city, setCity] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
+  const handleReset = () => {
+    setIsLoading(true);
+    setError("");
+  };
+
+  // pretend need to fetch for specific id - often case for external apis
   useEffect(() => {
     const url = import.meta.env.VITE_API_URL;
     const controller = new AbortController();
-    async function fetchData() {
-      setIsLoading(true);
-      setError("");
+
+    const fetchCity = async () => {
+      handleReset();
       try {
-        const response = await fetch(url, {
+        const response = await fetch(`${url}/${id}`, {
           signal: controller.signal,
         });
-        if (!response.ok) {
-          throw new Error("Error fetching data");
-        }
+        if (!response.ok) throw new Error("Error fetching data");
         const data = await response.json();
-        setCities(data);
+        setCity(data);
       } catch (e) {
         if (e.name !== "AbortError") setError(e.message);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
 
-    fetchData();
+    fetchCity();
 
     return () => controller.abort();
-  }, []);
+  }, [id]);
 
-  return { cities, isLoading, error };
+  return { city, isLoading, error };
 }
