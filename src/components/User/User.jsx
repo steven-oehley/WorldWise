19233e63f3538faf48ceeb5ba3efb0d65504d326
Steve <steve.gitprofile@outlook.com>
2024-08-28 12:49:33 +0200

    removed unused fake user from User

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,12 +1,4 @@
 import styles from "./User.module.css";
-
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 import { useAuth } from "../../context/useAuth";
 
 import { useNavigate } from "react-router-dom";
@@ -30,13 +22,3 @@ function User() {
 }
 
 export default User;
-
-/*
-CHALLENGE
-
-1) Add `AuthProvider` to `App.jsx` - done
-2) In the `Login.jsx` page, call `login()` from context - done
-3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app`
-4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
-5) Handle logout button by calling `logout()` and navigating back to `/`
-*/
